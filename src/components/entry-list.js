2acbcb78bd4c521b039f49ae88d3201170234818
Sean <seanphelan97@gmail.com>
2018-10-14 16:35:45 -0600

    add basic entry cards

--- src/components/entry-list.js
+++ src/components/entry-list.js
@@ -1,11 +1,25 @@
 
 import React from 'react';
+import './entry-list.css';
 
 export default function EntryList() {
   // FIXME:
-  const dummyData = ['a', 'b', 'c'];
-  // TODO missing keys
-  const entries = dummyData.map(element => <li><div>element</div></li>);
+  const dummyData = [
+    {preview: 'Lorem ipsum dolor sit amet', date:'10/12/2018', id:1},
+    {preview: 'second post', date:'10/13/2018', id:2},
+    {preview: 'third post', date:'10/14/2018', id:3}
+  ];
+  
+  const entries = dummyData.map(element => {
+    return (
+      <li key={element.id}>
+        <div class="entry-card">
+          <span class="entry-date">{element.date}</span>
+          <span class="entry-preview">{element.preview}</span>
+        </div>
+      </li>
+    );
+  });
   return (
     <React.Fragment>
       <h2>Entry list</h2>
