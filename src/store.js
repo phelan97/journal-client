64fff4a8f53b6fb47a0bc3414cce8fe689625090
Sean Phelan <seanphelan97@gmail.com>
2018-10-16 21:31:26 -0600

    update store, add local storage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,24 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {mainReducer} from './reducer';
 import thunk from 'redux-thunk';
 
- export default createStore(mainReducer, applyMiddleware(thunk));
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected';
+import {loadAuthToken, setAuthToken, refreshAuthToken} from './local-storage';
+
+const store = createStore(
+    combineReducers({
+        auth: authReducer,
+        protectedData: protectedDataReducer
+    }),
+    applyMiddleware(thunk)
+);
+
+// Try to grab authToken from localStorage
+const authToken = loadAuthToken();
+if (authToken) {
+    const token = authToken;
+    store.dispatch(setAuthToken(token));
+    store.dispatch(refreshAuthToken());
+}
+
+export default store;
