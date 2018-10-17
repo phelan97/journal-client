5eff4785c2c72d7710fcd3cefe137d2879ceb76e
Sean Phelan <seanphelan97@gmail.com>
2018-10-16 21:56:40 -0600

    add incomplete action and reducer files

---
+++ actions/auth.js
@@ -0,0 +1,16 @@
+
+const SET_AUTH_TOKEN = "";
+
+const CLEAR_AUTH = "";
+
+const AUTH_REQUEST = "";
+
+const AUTH_SUCESS = "";
+
+const AUTH_ERROR = "";
+
+const storeAuthInfo
+
+export const login
+
+export const refreshAuthToken
---
+++ actions/protected.js
@@ -0,0 +1,6 @@
+
+const FETCH_PROTECED_DATA_SUCESS = "";
+
+const FETCH_PROTECTED_DATA_ERROR = "";
+
+export default const fetchProtectedData
