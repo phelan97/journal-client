95ca6fd56b77fc53bacf3ae3ec323ebdb1c83d3a
Sean Phelan <seanphelan97@gmail.com>
2018-12-28 18:12:05 -0700

    change redux devtools setup

--- src/store.js
+++ src/store.js
@@ -1,14 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {reducer as formReducer} from 'redux-form';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-
 import authReducer from './reducers/auth';
 import userDataReducer from './reducers/user-data';
 import pageReducer from './reducers/page';
 import {loadAuthToken} from './local-storage';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     combineReducers({
         auth: authReducer,
@@ -16,7 +15,7 @@ const store = createStore(
         page: pageReducer,
         form: formReducer
     }),
-    composeWithDevTools(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 //Try to grab authToken from localStorage
